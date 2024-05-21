908a39005100e39abf06ec06f8bcc001025d5b32
AlunoJoaoVitorSchmidt <jvs0394@gmail.com>
2024-05-21 19:31:23 -0300

    0.4

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginForm.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (email === 'admin' && password === 'admin') {
+      navigate('/os');
+    } else {
+      alert('Credenciais inválidas');
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      /><br />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      /><br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default LoginForm;
---
+++ frontend/src/components/OSPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OSPage() {
+  return (
+    <div>
+      <h1>Bem-vindo à Página de OS's!</h1>
+    </div>
+  );
+}
+
+export default OSPage;
